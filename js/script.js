f420e60cb1e69620664b6526a3c9261123a5f4ca
AlbertoOrlando <albertoorlandowork@gmail.com>
2025-01-22 15:37:49 +0100

    eserczio con immagazzinamento dati in array

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
 const listaEmailRan = document.getElementById("listaEmail");
+let arrayEmail = [];
+
 
 for(let i = 0; i < 10; i++){
     axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
     .then(response => {
         const result = response.data;
+        arrayEmail.push(result.response)
         // console.log(result.response);
-        listaEmailRan.innerHTML += `
-            <li>${result.response}</li>
-        `;
+    
+        if(arrayEmail.length == 10)console.log(arrayEmail);
+        
     })
     .catch(error => {
         console.error(error)
